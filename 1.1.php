d1b5598e60d4a74cc0c0bcaff66765cfa20c4edd
Романова Диана Романовна <dromanova@ktk.local>
2022-09-13 10:15:41 +0300

    111
    111

---
+++ 1.1.php
@@ -0,0 +1,15 @@
+﻿'''lang:php;;
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta http-equiv="X-UA-Compatidle" content="IE=edge" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Создание РНР-скриптов</title>
+</head>
+<body>
+<?php
+echo ' Первая программа на РНР!';
+?>
+</body>
+</html>
+'''
---
+++ 1.2.php
@@ -0,0 +1,18 @@
+﻿'''lang:php;;
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Создание РНР-скриптов</title>
+</head>
+<body>
+<?php
+$dat = date('d.m.Y');
+$tm = date('h:i:s');
+echo 'Текущая дата: '.$dat.'<hr /'>;
+echo 'Текущее время'.$tm. '<hr />';
+?>
+</body>
+</html>
+'''
